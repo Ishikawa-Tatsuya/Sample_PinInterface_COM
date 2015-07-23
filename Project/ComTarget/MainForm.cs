f5452089049bc7675c67c8df29105a4be030b13d
ishikawa-tatsuya <ishikawa-tatsuya@codeer.co.jp>
2015-07-23 23:13:21 +0900

    作成

---
+++ Project/ComTarget/MainForm.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace ComTarget
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            var web = new WebBrowser();
+            web.Dock = DockStyle.Fill;
+            Controls.Add(web);
+        }
+    }
+}
---
+++ Project/ComTest/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading;
+using mshtml;
+using Codeer.Friendly;
+using VSHTC.Friendly.PinInterface;
+
+namespace ComTest
+{
+    [TestClass]
+    public class Sample
+    {
+        WindowsAppFriend app;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            app = new WindowsAppFriend(Process.Start("ComTarget"));
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Process.GetProcessById(app.ProcessId).CloseMainWindow();
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            var web = app.Type().System.Windows.Forms.Application.OpenForms[0].Controls[0];
+            web.Url = new Uri("http://www.codeer.co.jp/");
+            while ((WebBrowserReadyState)web.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Thread.Sleep(10);
+            }
+
+            var iframeWindow = web.Document.Window;
+            AppVar com = iframeWindow.DomWindow;
+
+            //インターフェイスを確定させる
+            var win = com.Pin<IHTMLWindow2>();
+            var doc = win.document.selection;
+            var type = doc.type;
+        }
+    }
+}
